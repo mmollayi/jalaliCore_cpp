d06b7a8b826c106d20450de2c15acec22a82660a
Mohsen <mmollayi@gmail.com>
2023-06-23 10:40:27 +0330

    updates

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,6 +5,10 @@ int main()
 {
     std::cout << "Hi there\n"; // invalid operator (<), extraneous semicolon, undeclared variable (x)
     int x {jalali_jd0_cpp(1401)};
+    int day;
+    int month;
+    int year;
+    day_to_dmy(2460118, &day, &month, &year);
     std::cout << x;
     return 0; // missing semicolon at end of statement
 }
--- jalali.h
+++ jalali.h
@@ -1,2 +1,3 @@
 int jalali_jd0_cpp(int jalali_year);
 int dmy_to_day(int day, int month, int year);
+void day_to_dmy(int jd, int *day, int *month, int *year);
